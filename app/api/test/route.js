cf7b96c90578b60d0ffe0dedc5ddebd2a904c6bf
asfand687 <asfandyar687@gmail.com>
2023-05-14 01:03:14 +0500

    deleted test route

--- app/api/test/route.js
+++
@@ -1,7 +0,0 @@
-export const GET = async (request) => {
-    try {
-        return new Response(JSON.stringify({message: "adventure is out there"}), { status: 200 })
-    } catch (error) {
-        return new Response("Failed to fetch all prompts", { status: 500 })
-    }
-} 
