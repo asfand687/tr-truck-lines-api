345c2c9abb5662d892d99d8852e52606fdaacbb1
asfand687 <asfandyar687@gmail.com>
2023-05-14 01:03:05 +0500

    deleted create route

--- app/api/create/route.js
+++
@@ -1,9 +0,0 @@
-export const POST = async (request) => {
-  const { username, password, email } = await request.json();
-
-  try {
-      return new Response(JSON.stringify({username, password, email}), { status: 201 })
-  } catch (error) {
-      return new Response("Failed to create a new prompt", { status: 500 });
-  }
-}
