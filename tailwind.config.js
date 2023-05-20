32276ee83d3800ff37216147e989cad2b00dedd3
Asfand Yar <asfandyar@novoassociates.com>
2023-05-20 22:35:43 +0500

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,18 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './sections/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        primary: "#1E73FB",
+        secondary: "#4DDE82",
+        light: "#EAF3F4",
+        accent: "#1CAEFC"
+      }
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
