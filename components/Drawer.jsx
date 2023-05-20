f51a621cc89c56ad90eb6936cb1f300735d94a47
Asfand Yar <asfandyar@novoassociates.com>
2023-05-20 23:56:14 +0500

    fixed drawer styles

--- components/Drawer.jsx
+++ components/Drawer.jsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { Link } from "react-scroll"
+
 const Drawer = ({ showDrawer, setShowDrawer }) => {
   return (
     <nav className={` md:hidden fixed z-50 top-20 bg-white p-6 flex flex-col items-center rounded-lg shadow-xl space-y-4 transition-all all ease-out duration-300 ${showDrawer ? 'right-0' : '-right-full'}`}>
@@ -12,19 +14,72 @@ const Drawer = ({ showDrawer, setShowDrawer }) => {
           alt="Close" 
         />
       </div>
-      <img 
-        className="w-32" 
-        src="/img/logo-new.svg" 
-        alt="Logo" 
-      />
-      <li>About Us</li>
-      <li>Our Vision</li>
-      <li>Pricing</li>
-      <button 
-        className="rounded font-semibold px-6 py-2 bg-primary text-light"
-      >
-        Get a Dispatcher
-      </button>
+      <Link
+          className="cursor-pointer"
+          to="home"
+          smooth={true}
+          duration={500}
+          offset={-50}
+        >
+        <img 
+          className="w-16" 
+          src="/img/logo-new.svg" 
+          alt="Logo" 
+        />
+      </Link>
+      <ul className="list-none">
+      <li>
+        <Link
+          offset={-85}
+          to="about"
+          smooth={true}
+          duration={500}
+          spy={true}
+          activeClass='font-semibold text-primary'
+          className="cursor-pointer"
+        >
+          About Us
+        </Link>
+      </li>
+      <li>
+        <Link
+          offset={-165}
+          to="vision"
+          smooth={true}
+          duration={500}
+          spy={true}
+          activeClass='font-semibold text-primary'
+          className="cursor-pointer"
+        >
+          Our Vision
+        </Link>
+      </li>
+      <li>
+        <Link
+          offset={-85}
+          to="pricing"
+          smooth={true}
+          duration={500}
+          activeClass='font-semibold text-primary'
+          className="cursor-pointer"
+          spy={true}
+          >
+            Pricing
+        </Link>
+      </li>
+      </ul>
+      <Link
+        offset={-115}
+        to="contact"
+        smooth={true}
+        duration={500}
+        className="cursor-pointer"
+        >
+        <button 
+          className="rounded font-semibold px-6 py-2 bg-primary text-light hover:opacity-80 transition-opacity ease-in-out duration-300">
+          Get a Dispatcher
+        </button>
+      </Link>
     </nav>
   )
 }
